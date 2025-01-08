07f4a11b99dc69924c1cfaeca41bc7f8f96f0dc2
GangDuo <5295036+GangDuo@users.noreply.github.com>
2025-01-08 17:49:03 +0900

    Saveボタンを削除する。

--- packages/export-form-fields/src/js/config.ts
+++ packages/export-form-fields/src/js/config.ts
@@ -5,11 +5,10 @@ import * as XLSX from "xlsx";
 // @ts-expect-error
 const PLUGIN_ID = kintone.$PLUGIN_ID;
 
-const form = document.querySelector(".js-submit-settings");
 const cancelButton = document.querySelector(".js-cancel-button");
 const messageInput =
   document.querySelector<HTMLInputElement>(".js-text-message");
-if (!(form && cancelButton && messageInput)) {
+if (!(cancelButton && messageInput)) {
   throw new Error("Required elements do not exist.");
 }
 const config = kintone.plugin.app.getConfig(PLUGIN_ID);
@@ -36,13 +35,6 @@ kintone.api(
   (err: any) => console.error(err)
 );
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  kintone.plugin.app.setConfig({ message: messageInput.value }, () => {
-    alert("The plug-in settings have been saved. Please update the app!");
-    window.location.href = "../../flow?app=" + kintone.app.getId();
-  });
-});
 cancelButton.addEventListener("click", () => {
   window.location.href = "../../" + kintone.app.getId() + "/plugin/";
 });
